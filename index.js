97985f5fc2c5b82d619a612d1c0bab5a8a8e0e74
wzd200 <william.z.dye@gmail.com>
2021-07-08 16:17:18 -0400

    Added build

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = express()
 
 server.use(express.json())
 server.use(cors())
-server.use(express.static(path.join('__dirname, client/')))
+server.use(express.static(path.join('__dirname, client/build')))
 const PORT = process.env.PORT || 5000
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
